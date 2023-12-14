7e74d646e57974763c9b8dd6212fd98b7515c144
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-14 20:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
+	 const lowercaseStr = text.toLowerCase();
+  const uppercaseStr = text.toUpperCase();
+  const result = `${lowercaseStr}-${uppercaseStr}`;
+  return result;
   // write your code here
+	
 }
 
 // DO not change the code below
